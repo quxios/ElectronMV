5674ad749781cafe4ea1dde3e9fd4fd5a2b738fa
Emilio Islas <exinyo@gmail.com>
2017-04-09 18:36:18 -0700

    update
    Clean syntax to match closer to RMMV style.
    Changed the start function to only grab width, height, x, y, and
    fullscreen from the winData.rpgsave
    Fixed bug where window wouldnt reposition if its saved x or y was 0
    Moved the window state saving here, was previously in the
    `electronRenderer.js`

--- app/js/electronMain.js
+++ app/js/electronMain.js
@@ -1,37 +1,42 @@
-const {app, BrowserWindow, ipcMain} = require('electron')
-const path = require('path')
-const url = require('url')
-const fs = require('fs')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
 
-const root = path.join(__dirname, '../')
-process.env.PRODUCTION = process.env.npm_package_name === undefined
-const devMode = (process.argv || []).indexOf('--dev') !== -1
-process.env.DEV = devMode
+const root = path.join(__dirname, '../');
+process.env.PRODUCTION = process.env.npm_package_name === undefined;
+const devMode = (process.argv || []).indexOf('--dev') !== -1;
+process.env.DEV = devMode;
 
-app.commandLine.appendSwitch('js-flags', '--expose_gc')
+app.commandLine.appendSwitch('js-flags', '--expose_gc');
 
-let win
+let win;
 
-const defaultWinDataPath = path.join(root, './data/WinData.json')
-let winDataPath = ''
+const defaultWinDataPath = path.join(root, './data/WinData.json');
+let winDataPath = '';
 if (path.extname(root) === '.asar') {
-  winDataPath = path.join(root, '../save/winData.rpgsave')
+  winDataPath = path.join(root, '../save/winData.rpgsave');
 } else {
-  winDataPath = path.join(root, './save/winData.rpgsave')
+  winDataPath = path.join(root, './save/winData.rpgsave');
 }
-let defaultWinData = JSON.parse(fs.readFileSync(defaultWinDataPath, 'utf8'))
-let winData = Object.assign({}, defaultWinData)
+let defaultWinData = JSON.parse(fs.readFileSync(defaultWinDataPath, 'utf8'));
+let winData = Object.assign({}, defaultWinData);
 
 function start () {
   fs.readFile(winDataPath, 'utf8', (err, data) => {
     if (!err) {
-      data = Buffer.from(data, 'base64').toString('utf8');
-      winData = Object.assign(JSON.parse(data), defaultWinData);
-      winData.fullscreen = JSON.parse(data).fullscreen;
+      data = JSON.parse(Buffer.from(data, 'base64').toString('utf8'));
+      winData = Object.assign({}, defaultWinData, {
+        width: data.width,
+        height: data.height,
+        x: data.x,
+        y: data.y,
+        fullscreen: data.fullscreen
+      });
     }
     if (!winData.width || !winData.height) {
-      winData.width = defaultWinData.res[0]
-      winData.height = defaultWinData.res[1]
+      winData.width = defaultWinData.res[0];
+      winData.height = defaultWinData.res[1];
     }
     createWindow();
   })
@@ -46,64 +51,69 @@ function createWindow () {
     webPreferences: {
       devTools: devMode && process.env.PRODUCTION !== 'true'
     }
-  })
+  });
 
   win.loadURL(url.format({
     pathname: path.join(root, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
-  win.setMenu(null)
-  win.setContentSize(winData.width, winData.height)
-  if (winData.x && winData.y) {
-    win.setPosition(winData.x, winData.y)
+  win.setMenu(null);
+  win.setContentSize(winData.width, winData.height);
+  if (!isNaN(winData.x) && !isNaN(winData.y)) {
+    win.setPosition(winData.x, winData.y);
   }
 
   win.on('closed', () => {
-    win = null
-  })
+    const data = Buffer.from(JSON.stringify(winData), 'utf8').toString('base64');
+    if (!fs.existsSync(path.dirname(winDataPath))) {
+      fs.mkdirSync(path.dirname(winDataPath));
+    }
+    fs.writeFileSync(winDataPath, data);
+    win = null;
+  });
   win.on('move', () => {
-    winData.x = win.getPosition()[0]
-    winData.y = win.getPosition()[1]
-  })
+    winData.x = win.getPosition()[0];
+    winData.y = win.getPosition()[1];
+  });
   win.on('resize', () => {
-    winData.width  = win.getContentSize()[0]
-    winData.height = win.getContentSize()[1]
-  })
+    winData.width  = win.getContentSize()[0];
+    winData.height = win.getContentSize()[1];
+  });
   win.webContents.openDevTools({
     mode: 'detach'
-  })
+  });
   win.webContents.once('devtools-opened', () => {
-    win.focus()
-  })
-  win.setFullScreen(winData.fullscreen || false)
+    win.focus();
+  });
+  win.setFullScreen(winData.fullscreen || false);
 }
 
-app.on('ready', start)
+app.on('ready', start);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (win === null) {
-    start()
+    start();
   }
-})
+});
 
 ipcMain.on('get-WinData', (e) => {
-  e.returnValue = Object.assign({}, winData, { fullscreen: win.isFullScreen() })
-})
+  e.returnValue = Object.assign({}, winData, { fullscreen: win.isFullScreen() });
+});
 
 ipcMain.on('open-DevTools', () => {
   win.openDevTools({
     detach: true
-  })
-})
+  });
+});
 
 ipcMain.on('set-fullscreen', (e) => {
-  win.setFullScreen(!win.isFullScreen())
-})
+  win.setFullScreen(!win.isFullScreen());
+});
